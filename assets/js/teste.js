10736426f9a52c811b8d7b0959b15c647b6275c0
leonardovff <leonardovff@gmail.com>
2016-11-04 16:27:27 -0300

    Testar camera

--- assets/js/teste.js
+++ assets/js/teste.js
@@ -1,14 +1,14 @@
 function testar(){
     get.item("#hash_pw").value = "liipzd";
-    app.login(function(){
+    // app.login(function(){
     return true; 
         get.item("#iniciar").click();
-        get.item("#inserir-manualmente").click();
-        app.resultadoLeitura("95012300000338");
-        testarResponder();
+        // get.item("#inserir-manualmente").click();
+        // app.resultadoLeitura("95012300000338");
+        // testarResponder();
         // AL - 2,3,5,3,5
         // BA - 1,4,5,3,5
-    });
+    // });
 }
 function testarResponder(){
     setTimeout(function(){
